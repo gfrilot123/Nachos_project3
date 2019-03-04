4bfd7ee6fa3aa2b6214f859128afa84ae7c9a3e6
gfrilot123 <30584458+gfrilot123@users.noreply.github.com>
2019-03-03 18:08:23 -0600

    Add files via upload

--- exception.cc
+++ exception.cc
@@ -1,4 +1,4 @@
-// exception.cc 
+// exception.cc
 //	Entry point into the Nachos kernel from user programs.
 //	There are two kinds of things that can cause control to
 //	transfer back to here from user code:
@@ -9,7 +9,7 @@
 //
 //	exceptions -- The user code does something that the CPU can't handle.
 //	For instance, accessing memory that doesn't exist, arithmetic errors,
-//	etc.  
+//	etc.
 //
 //	Interrupts (which can also cause control to transfer from user
 //	code into the Nachos kernel) are handled elsewhere.
@@ -18,7 +18,7 @@
 // Everything else core dumps.
 //
 // Copyright (c) 1992-1993 The Regents of the University of California.
-// All rights reserved.  See copyright.h for copyright notice and limitation 
+// All rights reserved.  See copyright.h for copyright notice and limitation
 // of liability and disclaimer of warranty provisions.
 
 #include <stdio.h>        // FA98
@@ -30,6 +30,7 @@
 
 // begin FA98
 
+void processCreator(int arg);
 static int SRead(int addr, int size, int id);
 static void SWrite(char *buffer, int size, int id);
 
@@ -49,12 +50,12 @@ static void SWrite(char *buffer, int size, int id);
 //		arg3 -- r6
 //		arg4 -- r7
 //
-//	The result of the system call, if any, must be put back into r2. 
+//	The result of the system call, if any, must be put back into r2.
 //
 // And don't forget to increment the pc before returning. (Or else you'll
 // loop making the same system call forever!
 //
-//	"which" is the kind of exception.  The list of possible exceptions 
+//	"which" is the kind of exception.  The list of possible exceptions
 //	are in machine.h.
 //----------------------------------------------------------------------
 
@@ -90,7 +91,38 @@ ExceptionHandler(ExceptionType which)
 			interrupt->Halt();
 			break;
 
-			
+		case SC_Exec:
+
+			/* write code for handling Exec */
+
+			int bufferAdd = machine->ReadRegister(4);
+			char *filename = new char[100];
+
+			if (!machine->ReadMem(bufferAdd, 1, &ch)) return;
+			int i = 0;
+			while (ch != 0)
+			{
+				filename[i] = (char)0;
+				bufferAdd += 1;
+				i++;
+				if (!machine->ReadMem(bufferAdd, 1, &i)) return;
+			}
+
+				filename[i] = (char)0;
+			/* now filename contains the file */
+
+			machine->WriteRegister(2,filename);
+
+			int pc;
+			pc=machine->ReadRegister(PCReg);
+			machine->WriteRegister(PrevPCReg,pc);
+			pc=machine->ReadRegister(NextPCReg);
+			machine->WriteRegister(PCReg,pc);
+			pc+=4;
+			machine->WriteRegister(NextPCReg,pc);
+
+			break;
+
 		case SC_Read :
 			if (arg2 <= 0 || arg3 < 0){
 				printf("\nRead 0 byte.\n");
@@ -107,7 +139,7 @@ ExceptionHandler(ExceptionType which)
 					j=j-1;
 				else{
 					ch[j] = (char) i;
-					if (ch[j] == '\0') 
+					if (ch[j] == '\0')
 						break;
 				}
 			}
@@ -121,9 +153,9 @@ ExceptionHandler(ExceptionType which)
 			break;
 
 			default :
-			//Unprogrammed system calls end up here
+			printf("Default location for undefined system calls! \n");
 			break;
-		}         
+		}
 		break;
 
 	case ReadOnlyException :
@@ -226,5 +258,13 @@ static void SWrite(char *buffer, int size, int id)
 	WriteFile(id,buffer,size);
 }
 
-// end FA98
+void processCreator(int arg)
+{
+	currentThread->space->InitRegisters();
+	currentThread->space->RestoreState();
 
+	machine->Run();
+	ASSERT(FALSE);
+}
+
+// end FA98
